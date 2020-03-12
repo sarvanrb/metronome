5e279a09e14b732608637b4824cb7c6cb2532a05
Saravanan <sarvan.rb@gmail.com>
2020-03-12 20:45:22 +0530

    Updates

---
+++ precache-manifest.2f0ce4f47ef526add4dce0e6f9fec6d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50866a9fe97b8a26a207ce88b25beeec",
+    "url": "/metronome/index.html"
+  },
+  {
+    "revision": "98e447f1ce184362b977",
+    "url": "/metronome/static/css/main.76d9d800.chunk.css"
+  },
+  {
+    "revision": "be3f3d4c0e24694b546d",
+    "url": "/metronome/static/js/2.94a74266.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/metronome/static/js/2.94a74266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e447f1ce184362b977",
+    "url": "/metronome/static/js/main.3ae7d259.chunk.js"
+  },
+  {
+    "revision": "e9b5206e0e05559a8fa6",
+    "url": "/metronome/static/js/runtime-main.dc1c8f84.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/metronome/static/media/click2.fe0ded65.wav"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/metronome/static/media/logo.5d5d9eef.svg"
+  }
+]);
